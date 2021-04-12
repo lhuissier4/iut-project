df69278b9ba2ac87947191c7987ca4083c7ed06d
Devin Ivy <devin@bigroomstudios.com>
2021-04-11 23:24:33 -0400

    erreur /documentation

--- server/manifest.js
+++ server/manifest.js
@@ -3,6 +3,7 @@
 const Dotenv = require('dotenv');
 const Confidence = require('@hapipal/confidence');
 const Toys = require('@hapipal/toys');
+const Schwifty = require('@hapipal/schwifty');
 
 // Pull .env into process.env
 Dotenv.config({ path: `${__dirname}/.env` });
@@ -34,7 +35,31 @@ module.exports = new Confidence.Store({
                 options: {}
             },
             {
-                plugin: './plugins/swagger'
+                plugin  : '@hapipal/schwifty',
+                options : {
+                    $filter    : 'NODE_ENV',
+                    $default   : {},
+                    $base      : {
+                        migrateOnStart : true,
+                        knex           : {
+                            client     : 'mysql',
+                            connection : {
+                                host     : process.env.DB_HOST || '0.0.0.0',
+                                user     : process.env.DB_USER || 'root',
+                                password : process.env.DB_PASSWORD || 'hapi',
+                                database : process.env.DB_DATABASE || 'user',
+                                port     : process.env.DB_PORT || 3307
+                            },
+                            migrations: {
+                                stub: Schwifty.migrationsStubPath
+                            }
+                        }
+                    },
+                    production : {
+                        migrateOnStart : false
+                    }
+
+                }
             },
             {
                 plugin: {
@@ -42,7 +67,23 @@ module.exports = new Confidence.Store({
                     $default: '@hapipal/hpal-debug',
                     production: Toys.noop
                 }
+            },
+            {
+                plugin: 'hapi-swagger',
+                options: {
+                    info: {
+                        title: 'API Documentation',
+                        version: '1.0.0'
+                    }
+                }
+            },
+            {
+                plugin: 'inert'
+            },
+            {
+                plugin: 'vision'
             }
+
         ]
     }
 });
